2f5b1aeb9beef0a0d4348566fb3c8cd4377cd18e
Ubuntu <ubuntu@ip-172-31-1-5.ap-south-1.compute.internal>
2025-01-24 06:26:22 +0000

    ansible roles

---
+++ roles/frontend/docker/src/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Multi-Tier Web Application</h1>
+      <p>This is the frontend running on React!</p>
+    </div>
+  );
+}
+
+export default App;
+
